e075981bd8deec95f978053b82566a6c40f13d64
César Chavez <cesarc99@gmail.com>
2024-07-09 14:40:38 -0300

    Sin back completado

--- JavaScript/Configuracion.js
+++ JavaScript/Configuracion.js
@@ -2,15 +2,11 @@
 
 var Books = [], unBook, Users = [], unUser;
 
-// document.getElementById("btnConfirmaUser").addEventListener("click", procUser);
-document.getElementById("btnConfirmaUser").addEventListener("click", procUserApi);
+document.getElementById("btnConfirmaUser").addEventListener("click", procUser);
 document.getElementById("btnConfirmaBook").addEventListener("click", procBook);
 
 document.getElementById("btnBorraUser").addEventListener("click", aliasResetUsuarios);
 document.getElementById("btnBorraBook").addEventListener("click", aliasResetLibros);
-
-document.getElementById("Usuario_Id").onchange = function() {dataById(event)};
-document.getElementById("Libro_Id").onchange = function() {dataById(event)};
 
 function aliasResetLibros() {
 	resetFormEvent("book_action", "FormLibros", "mensajeErrorLibro", false);
@@ -53,13 +49,6 @@ function listSection(Section) {
     for (let i = 0; i < Books.length; i++){
       unBook = Books[i];
 	  tableBooks += "<tr><td>" + unBook["Id"] + "</td><td>" + unBook["Titulo"] + "</td><td>" +  unBook["Ubicacion"] + "</td></tr>";
-/*
-      nodoBook = nodoArticle.children[0].cloneNode(true);
-      nodoBook.children[0].innerText = unBook["Id"];
-      nodoBook.children[1].innerText = unBook["Titulo"];
-      nodoBook.children[2].innerText = unBook["Ubicacion"];
-      nodoArticle.appendChild(nodoBook);
-*/
 	}
 	tableBooks += "</table>";
 	nodoArticle.innerHTML = tableBooks;
@@ -68,25 +57,9 @@ function listSection(Section) {
     for (let i = 0; i < Users.length; i++){
       unUser = Users[i];
 	  tableUsers += "<tr><td>" + unUser["Id"] + "</td><td>" + unUser["NomApe"] + "</td><td>" +  unUser["Contacto"] + "</td></tr>";
-/*
-      nodoUser = nodoArticle.children[0].cloneNode(true);
-      nodoUser.children[0].innerText = unUser["Id"];
-      nodoUser.children[1].innerText = unUser["NomApe"];
-      nodoUser.children[2].innerText = unUser["Contacto"];
-      nodoArticle.appendChild(nodoUser);
-*/
 	}
 	tableUsers += "</table>";
 	nodoArticle.innerHTML = tableUsers;
-  }
-}
-
-
-function delSection(Section) {
-  const nodoSection = document.getElementById(Section);
-  const nodoArticle = nodoSection.children[1];
-  while (nodoArticle.children.length > 1) {
-    nodoArticle.removeChild(nodoArticle.lastChild);
   }
 }
 
@@ -127,52 +100,10 @@ function procUser() {
 	msg.style.color = "white";
 	objBtn.checked = false;
 	aliasResetUsuarios();
-//    delSection("Usuarios");
     listSection("Usuarios");
   } else {
 	msg.style.color = "rgba(240, 7, 7, 0.74)";
   }
-}
-
-function procUserApi() {
-  var respuesta = {"msg": "", "sts": 0};
-  var unUser = new User(0, "", "", "");
-  var objBtn, msg;
-  msg = document.getElementById('mensajeErrorUsuario');
-  unUser["Id"]         = document.getElementById("Usuario_Id").value;
-  if (document.getElementById('del_U').checked || document.getElementById('upd_U').checked) {
-	alert(unUser["Id"]);
-  }	
-/*
-  unUser["NomApe"]     = document.getElementById("Usuario_NomApe").value;
-  unUser["Direccion"]  = document.getElementById("Usuario_Direccion").value;
-  unUser["Contacto"]   = document.getElementById("Usuario_Contacto").value;
-    
-  if (document.getElementById('add_U').checked) {
-	  objBtn = document.getElementById('add_U');
-	  respuesta = add(Users, "User", unUser);
-  } else if (document.getElementById('del_U').checked) {
-	  objBtn = document.getElementById('del_U');
-	  respuesta = del(Users, "User", unUser["Id"]);
-  } else if (document.getElementById('upd_U').checked) {
-	  objBtn = document.getElementById('upd_U');
-	  respuesta = upd(Users, "User", unUser);
-  } else {
-	  respuesta.msg = "Debe seleccionar una operación (A, B o M)";
-	  respuesta.sts = 1;
-  }
-  
-  msg.innerText = respuesta.msg;
-  if (respuesta.sts == 0) {
-	msg.style.color = "white";
-	objBtn.checked = false;
-	aliasResetUsuarios();
-//    delSection("Usuarios");
-    listSection("Usuarios");
-  } else {
-	msg.style.color = "rgba(240, 7, 7, 0.74)";
-  }
-*/
 }
 
 
@@ -206,7 +137,6 @@ function procBook() {
 	msg.style.color = "white";
 	objBtn.checked = false;
 	aliasResetLibros();
-//    delSection("Libros");
     listSection("Libros");
   } else {
 	msg.style.color = "rgba(240, 7, 7, 0.74)";
